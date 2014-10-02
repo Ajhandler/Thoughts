e80075a0e61a9817c8c81096bba7b0329a0651cd
Emerson Scherer <emerson@emersonscherer.com>
2014-10-02 15:44:20 -0400

    edited main.rb

--- main.rb
+++ main.rb
@@ -4,3 +4,18 @@ require './models'
 
 
 set :database, "sqlite3:thoughts.sqlite3"
+
+enable :sessions
+set :sessions => true
+
+def current_user
+	if session[:user_id]
+		@current_user = User.find(session[:user_id])
+	end
+end
+
+get '/' do 
+	erb :home
+	
+end
+
